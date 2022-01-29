c2a94411f1262fa00d63274448cd1a61f32191b8
tkgka <ksh17171@gmail.com>
2022-01-29 15:33:58 +0900

    Initial commit

---
+++ Example/AlertPopup/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AlertPopup
+//
+//  Created by tkgka on 01/29/2022.
+//  Copyright (c) 2022 tkgka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/AlertPopup/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AlertPopup
+//
+//  Created by tkgka on 01/29/2022.
+//  Copyright (c) 2022 tkgka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
